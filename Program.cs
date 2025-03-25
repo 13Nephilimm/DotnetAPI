557e2597e92f09854d93639073594f034bd85dd6
13Nephilimm <jekokharabadze@gmail.com>
2025-03-25 03:13:01 -0700

    jygj

---
+++ Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((options) =>
+    {
+        options.AddPolicy("DevCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8080")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+ 
+var app = builder.Build();
+ 
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCors");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
+
+app.MapControllers();   
+
+app.Run();
+
